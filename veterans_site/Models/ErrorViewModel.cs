d56e0906e4cb2eca00aa27d6ccf99e206e558e48
HorobetsDmytro <dmtrgorobec@gmail.com>
2024-10-13 23:56:54 +0300

    Добавьте файлы проекта.

---
+++ veterans_site/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace veterans_site.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
